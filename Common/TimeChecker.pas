958f0a66e6554b53578aab0d4ea33ee055545aca
a3v <a3vinogradov@gmail.com>
2020-03-11 11:12:25 +0300

    2

--- Common/TimeChecker.pas
+++ Common/TimeChecker.pas
@@ -1,11 +1,47 @@
 unit TimeChecker;
 
 interface
+uses
+  SysUtils, System.DateUtils;
+
 type
   TTimeChecker = class
   private
-
+    _timeStart: TDateTime;
+    _timeStop: TDateTime;
+    _active: Boolean;
+    procedure SetActive(const Value: Boolean);
+  public
+    procedure Start;
+    procedure Stop;
+    function Report: String;
+  published
+    property TimeStart: TDateTime read _timeStart write _timeStart;
+    property TimeStop: TDateTime read _timeStop write _timeStop;
+    property Active: Boolean read _active write SetActive;
   end;
 implementation
+
+{ TTimeChecker }
+
+function TTimeChecker.Report: String;
+begin
+  Result := Format('Время выполнения %d секунд.', [SecondsBetween(TimeStop, TimeStart)]);
+end;
+
+procedure TTimeChecker.SetActive(const Value: Boolean);
+begin
+  _active := Value;
+end;
+
+procedure TTimeChecker.Start;
+begin
+  TimeStart := Now();
+end;
+
+procedure TTimeChecker.Stop;
+begin
+  TimeStop := Now();
+end;
 
 end.
--- FileGenerator/FileGenerator.dpr
+++ FileGenerator/FileGenerator.dpr
@@ -7,15 +7,18 @@ uses
   TimeChecker in '..\Common\TimeChecker.pas';
 
 const
-  Imax = 100;
+  Imax = 10000000;
 var
-  i: integer;
-  s: string;
+  i: int64;
   strList: TStringList;
+  tch: TTimeChecker;
+  s: string;
 begin
+  strList := TStringList.Create;
+  tch := TTimeChecker.Create;
   try
     try
-      TTimeChecker.Start();
+      tch.Start();
       strList := TStringList.Create;
       Randomize();
       for i := 0  to Imax do
@@ -23,13 +26,15 @@ begin
         strList.Add(Format('%d. %s%d',[Random(65535),'test string',Random(65535)]));
       end;
       strList.SaveToFile('data.txt');
-      TTimeChecker.Stop();
-      WriteLn(TTimeChecker.)
+      tch.Stop();
+      WriteLn(tch.Report());
     except
       on E: Exception do
         Writeln(E.ClassName, ': ', E.Message);
     end;
   finally
     FreeAndNil(strList);
+    FreeAndNil(tch);
   end;
+  Readln(s);
 end.
